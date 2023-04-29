0851f2fd75e92fd66bab20f87e58ed98a69b8487
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2023-04-29 11:21:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/bot.js";
+import Chat from "./components/bot.js";
 import Index from "./components/Index";
 import "./components/all.css";
 
@@ -8,7 +8,7 @@ const App = () => {
         
         <Router>
             <Route exact path="/" component={Index} />
-            <Route path="/home" component={Home} />
+            <Route path="/chat" component={Chat} />
         </Router>
         
     )
